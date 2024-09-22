cbe84831061356446843889bb3f6dfcebb213d7c
kuroness007 <dogdog.yusuke24@gmail.com>
2024-09-23 04:31:57 +0900

    add log print

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    print(message)
+    await message.channel.send(message)
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
